32c38208f3237a9b4282a8e863234753c4a179ed
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2024-01-17 23:21:23 +0300

    Task 17.6.6

---
+++ Modul17/Modul17/UsualCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul17
+{
+    public class UsualCalculator : ICalculator
+    {
+        public void CalculateInterest(Account account)
+        {
+                // расчет процентной ставки обычного аккаунта по правилам банка
+                account.Interest = account.Balance * 0.4;
+
+            if (account.Balance < 1000)
+                account.Interest -= account.Balance * 0.2;
+            else
+                account.Interest -= account.Balance * 0.4;
+        }
+    }
+}
